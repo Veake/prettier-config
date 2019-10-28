fa7887829c6e960a4695351bdeaab7d7e159db13
m-ripper <max@jovo.tech>
2019-10-28 18:29:57 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "consistent",
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "strict"
+};
